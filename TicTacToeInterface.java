22bb026070163e40d73a22d11fadc2d8525cb0fd
AriasBueno <madelinarias@mail.com>
2017-08-02 19:23:22 -0700

    Create TicTacToeInterface.java

---
+++ TicTacToeInterface.java
@@ -0,0 +1,15 @@
+package Teacher_lec20;
+
+public interface TicTacToeInterface {
+
+
+	public void displayBoard();	
+	public void clearBoard();
+	public void displayWinner();
+	public void takeATurn();
+	public void updateCurrPlayer();
+	public boolean isFull();
+
+
+
+}
